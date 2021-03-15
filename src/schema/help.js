a5ef0417b2f4090de21f7fc31059553bdb6104f4
Kasper F <kfu@dbc.dk>
2021-03-15 16:10:26 +0100

    added possibility for searching in help articles

---
+++ src/schema/help.js
@@ -0,0 +1,22 @@
+/**
+ * @file Help type definition and resolvers
+ *
+ */
+
+export const typeDef = `
+type HelpRow {
+  nid: Int!
+  title: String!
+  body: String!
+}
+type HelpResponse {
+  result: [HelpRow!]!
+}`;
+
+export const resolvers = {
+  HelpResponse: {
+    async result(parent, args, context, info) {
+      return await context.datasources.helptexts.load(parent.q);
+    }
+  }
+};
