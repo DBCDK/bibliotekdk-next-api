5e56be1c547a2e19ed90e73b9f9b11ff33c19187
Kasper Fuglsang <kfu@dbc.dk>
2022-04-25 15:08:49 +0200

    added lookup by faust, pid, id in draft

--- src/schema/draft/manifestation.js
+++ src/schema/draft/manifestation.js
@@ -669,42 +669,4 @@ union Draft_Access = Draft_URL | Draft_Ereol | Draft_Ill | Draft_InfomediaServic
 
 `;
 
-export const resolvers = {
-  // Draft_Manifestation: {
-  //   title(parent, args, context) {
-  //     return {
-  //       main: "Some Title",
-  //       full: "Some Title: Full",
-  //       parallel: ["Parallel Title 1", "Parallel Title 2"],
-  //       sort: "Some Title Sort",
-  //       original: "Some Title Standard",
-  //     };
-  //   },
-  //   access() {
-  //     return [
-  //       {
-  //         __typename: "Draft_URL",
-  //         origin: "dbcwebarkiv",
-  //         url: "https://moreinfo.dbc.dk",
-  //       },
-  //       {
-  //         __typename: "Draft_Ill",
-  //         ill: true,
-  //       },
-  //       {
-  //         __typename: "Draft_InfomediaService",
-  //         id: "123456",
-  //       },
-  //       {
-  //         __typename: "Draft_DigitalArticleService",
-  //         issn: "123456",
-  //       },
-  //     ];
-  //   },
-  // },
-  // Draft_Work: {
-  //   manifestations() {
-  //     return [{}, {}];
-  //   },
-  // },
-};
+export const resolvers = {};
