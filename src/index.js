416cf3c6ca83ff8934fa4e37f7391e9297a2842e
Kasper Fuglsang <kfu@dbc.dk>
2022-09-06 12:34:22 +0200

    refactor

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import express from "express";
 import { createProxyMiddleware } from "http-proxy-middleware";
 
 import cors from "cors";
-import { graphqlHTTP } from "express-graphql";
+import { graphqlHTTP, getGraphQLParams } from "express-graphql";
 import { parse, getOperationAST, print } from "graphql";
 import config from "./config";
 import howruHandler from "./howru";
@@ -37,6 +37,8 @@ promExporterApp.listen(9599, () => {
 
 (async () => {
   app.use(cors());
+  // Set limit on body size
+  app.use(express.json({ limit: 10000 }));
 
   // trust ip-addresses from X-Forwarded-By header, and log requests
   app.enable("trust proxy");
@@ -53,6 +55,7 @@ promExporterApp.listen(9599, () => {
     };
     next();
   });
+
   // Middleware that monitors performance of those GraphQL queries
   // which specify a monitor name.
   app.post("/:agency/:profile/graphql", async (req, res, next) => {
@@ -96,6 +99,92 @@ promExporterApp.listen(9599, () => {
   });
 
   /**
+   * Middleware for initializing dataloaders
+   */
+  app.post("/:agency/:profile/graphql", async (req, res, next) => {
+    req.datasources = createDataLoaders(uuid());
+    next();
+  });
+
+  /**
+   * Middleware for validating access token, and fetching smaug configuration
+   */
+  app.post("/:agency/:profile/graphql", async (req, res, next) => {
+    // Get graphQL params
+    const graphQLParams = await getGraphQLParams(req);
+    const document = parse(graphQLParams.query);
+    const ast = getOperationAST(document);
+    req.queryVariables = graphQLParams.variables;
+    req.parsedQuery = graphQLParams.query
+      .replace(/\n/g, " ")
+      .replace(/\s+/g, " ");
+
+    // Check if query is introspection query
+    req.isIntrospectionQuery = isIntrospectionQuery(ast);
+
+    // Get bearer token from authorization header
+    req.accessToken =
+      req.headers.authorization &&
+      req.headers.authorization.replace(/bearer /i, "");
+
+    // Fetch Smaug client configuration
+    try {
+      req.smaug =
+        req.accessToken &&
+        (await req.datasources.smaug.load({
+          accessToken: req.accessToken,
+        }));
+      req.smaug.app.ips = (req.ips.length && req.ips) || [req.ip];
+
+      // Agency of the smaug client
+      const agency = req.smaug?.agencyId;
+
+      req.profile = {
+        agency,
+        name: req.params.profile,
+        combined: `${agency}/${req.params.profile}`,
+      };
+    } catch (e) {
+      if (e.response && e.response.statusCode !== 404) {
+        log.error("Error fetching from smaug", { response: e });
+        res.status(500);
+        return res.send({
+          statusCode: 500,
+          message: "Internal server error",
+        });
+      }
+    }
+
+    // If query is introspection, we allow access even though
+    // No token is given
+    if (!req.isIntrospectionQuery) {
+      // Invalid access token
+      if (!req.smaug) {
+        res.status(403);
+        return res.send({
+          statusCode: 403,
+          message: "Unauthorized",
+        });
+      }
+
+      // Access token is valid, but client is not configured properly
+      if (!req.profile?.agency) {
+        log.error(
+          `Missing agency in configuration for client ${req.smaug?.app?.clientId}`
+        );
+        res.status(403);
+        return res.send({
+          statusCode: 403,
+          message:
+            "Invalid client configuration. Missing agency in configuration for client.",
+        });
+      }
+    }
+
+    next();
+  });
+
+  /**
    * Check if operation is introspection
    * @param {*} operation
    * @returns
@@ -111,41 +200,6 @@ promExporterApp.listen(9599, () => {
   app.post(
     "/:agency/:profile/graphql",
     graphqlHTTP(async (request, response, graphQLParams) => {
-      request.profile = {
-        agency: request.params.agency,
-        name: request.params.profile,
-        combined: `${request.params.agency}/${request.params.profile}`,
-      };
-      // Create dataloaders and add to request
-      request.datasources = createDataLoaders(uuid());
-
-      request.queryVariables = graphQLParams.variables;
-      request.parsedQuery = graphQLParams.query
-        .replace(/\n/g, " ")
-        .replace(/\s+/g, " ");
-
-      // Get bearer token from authorization header
-      request.accessToken =
-        request.headers.authorization &&
-        request.headers.authorization.replace(/bearer /i, "");
-
-      // Fetch smaug configuration
-      try {
-        request.smaug =
-          request.accessToken &&
-          (await request.datasources.smaug.load({
-            accessToken: request.accessToken,
-          }));
-        request.smaug.app.ips = (request.ips.length && request.ips) || [
-          request.ip,
-        ];
-      } catch (e) {
-        if (e.response && e.response.statusCode !== 404) {
-          log.error("Error fetching from smaug", { response: e });
-          throw "Internal server error";
-        }
-      }
-
       return {
         schema: await getExecutableSchema({
           clientPermissions: request?.smaug?.gateway,
@@ -161,23 +215,6 @@ promExporterApp.listen(9599, () => {
           }
         },
         validationRules: [
-          function authenticate() {
-            if (request.method === "GET") {
-              throw "Unauthorized";
-            }
-
-            const document = parse(graphQLParams.query);
-            const ast = getOperationAST(document);
-
-            // If this is not the introspection query,
-            // a valid token is required
-            if (!request.smaug && !isIntrospectionQuery(ast)) {
-              throw "Unauthorized";
-            }
-
-            // All ok
-            return { Field() {} };
-          },
           validateComplexity({
             query: graphQLParams.query,
             variables: graphQLParams.variables,
@@ -195,6 +232,19 @@ promExporterApp.listen(9599, () => {
   app.get("/howru", howruHandler);
 
   app.use(proxy);
+
+  // Default error handler
+  app.use((error, request, response, next) => {
+    if (error) {
+      log.error(String(error), {
+        error: String(error),
+        stacktrace: error.stack,
+      });
+      response.status(500).send({ error: "Internal server error" });
+    } else {
+      next();
+    }
+  });
 
   server = app.listen(config.port, () => {
     log.info(`Running GraphQL API at http://localhost:${config.port}/graphql`);
